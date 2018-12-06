f29a81b7df855e936bd31524d4b13662183e426e
LluisaMarsal <llmarsal@intelligenter.org>
2018-12-06 19:51:43 +0100

    second go

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -4,11 +4,13 @@ contract RockPaperScissors {
     
     address public owner;
     
+    enum Bet {ROCK, PAPER, SCISSORS}
+    
     struct BetBox {
        address player1;
        address player2;
-       uint betPlayer1;
-       uint betPlayer2;
+       Bet betPlayer1;
+       Bet betPlayer2;
        uint amountPlayer1;
        uint amountPlayer2;
        uint sumAmounts; 
@@ -16,34 +18,63 @@ contract RockPaperScissors {
     }
     
     // for every uint there is a BetBox and those namespaces (struct) will conform a mapping named wagerStructs
-    mapping (bytes32 => BetBox) public wagerStructs; 
+    mapping (bytes32 => BetBox) public betStructs; 
     
-    //event Log
-    //event Log
+    event LogCreateBet(address caller, uint amount, uint duration);
+    event LogSeeBet(address caller, uint amount, uint now);
+    event LogPlayBet(address winner, uint sumAmounts, uint now);
     
     function RockPaperScissors() public {
         owner = msg.sender;
     }
     
-    function hashBet(bytes32 passPlayer1, bytes32 passPlayer2) public pure returns(bytes32 hashedBet) {
+    function gethashToBet(bytes32 passPlayer1, bytes32 passPlayer2) public pure returns(bytes32 hashToBet) {
         return keccak256(passPlayer1, passPlayer2);
     }
     
-    function playBet(bytes32 passPlayer1, bytes32 passPlayer2, uint betPlayer1, uint betPlayer2) public returns(bool success) {
-        bytes32 hashedBet = hashBet(passPlayer1, passPlayer2);
-        BetBox memory b = wagerStructs[hashedBet];
+    function createBet(bytes32 hashToBet, address player2, uint duration) public payable returns(bool success) {
+        if(betStructs[hashToBet].sumAmounts != 0) revert();
+        require(betStructs[hashToBet].player1 != player2); 
+        betStructs[hashToBet].deadline = duration + block.number;
+        betStructs[hashToBet].amountPlayer1 = amount;
+        uint amount = betStructs[hashToBet].amountPlayer1;
+        LogCreateBet(msg.sender, msg.value, duration);
+        return true;
+    }
+    
+    function seeBet(bytes32 hashToBet, address player1) public payable returns(bool success) {
+        if(betStructs[hashToBet].sumAmounts != 0) revert();
+        require(betStructs[hashToBet].player2 != player1); 
+        require(betStructs[hashToBet].deadline < now);
+    //QUESTION TO REVIEWERS: this require() does not work as it allows player2 to bet below player1, why isn't working?
+        require(betStructs[hashToBet].amountPlayer1 == betStructs[hashToBet].amountPlayer2);
+        betStructs[hashToBet].amountPlayer2 = amount;
+        uint amount = betStructs[hashToBet].amountPlayer2;
+        LogSeeBet(msg.sender, msg.value, now);
+        return true;
+    }
+    
+    function playBet(bytes32 passPlayer1, bytes32 passPlayer2, Bet betPlayer1, Bet betPlayer2) public returns(address winner) {
+        bytes32 hashToBet = gethashToBet(passPlayer1, passPlayer2);
+        BetBox memory b = betStructs[hashToBet];
         require(b.amountPlayer1 + b.amountPlayer2 == b.sumAmounts);
         if (b.player1 == b.player2) revert();
         if (b.betPlayer1 == b.betPlayer2) revert();
-        //my idea here was to assign numbers to the different betting options, (eg.rock = 1, scissors = 2, paper = 3) 
-        //then start comparing the bets to find the winner but seems I am not using the right expression, or maybe this 
-        //is not the right strategy at all?
-        if (b.betPlayer1=1 && b.betPlayer2=2) {
-            b.player1.transfer(b.sumAmounts);
-        }
-        
-        //Log
-        //return
-
+        if ((betPlayer1 == Bet.PAPER && betPlayer2 == Bet.ROCK)||
+            (betPlayer1 == Bet.ROCK && betPlayer2 == Bet.SCISSORS)||
+            (betPlayer1 == Bet.SCISSORS && betPlayer2 == Bet.PAPER)||
+            (betPlayer1 == Bet.ROCK && betPlayer2 == Bet.SCISSORS)||
+            (betPlayer1 == Bet.PAPER && betPlayer2 == Bet.ROCK)||
+            (betPlayer1 == Bet.SCISSORS && betPlayer2 == Bet.PAPER))
+            return b.player1;
+        if ((betPlayer2 == Bet.PAPER && betPlayer1 == Bet.ROCK)||
+            (betPlayer2 == Bet.ROCK && betPlayer1 == Bet.SCISSORS)||
+            (betPlayer2 == Bet.SCISSORS && betPlayer1 == Bet.PAPER)||
+            (betPlayer2 == Bet.ROCK && betPlayer1 == Bet.SCISSORS)||
+            (betPlayer2 == Bet.PAPER && betPlayer1 == Bet.ROCK)||
+            (betPlayer2 == Bet.SCISSORS && betPlayer1 == Bet.PAPER))
+            return b.player2;    
+        LogPlayBet(winner, b.sumAmounts, now);
+        winner.transfer(b.sumAmounts);
     }
 }
