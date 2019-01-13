2194867fc196e9ce944e14de061d4f2c9fef59bd
LluisaMarsal <llmarsal@intelligenter.org>
2019-01-13 18:10:16 +0100

    Good citizen actions, removing redundancies and owner's participation

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.4.19;
  
 contract RockPaperScissors {
     
-    address public owner;
+    address private owner;
+    uint constant maxNextNumberOfBlocks = 1 days / 15;
+    uint constant minNextNumberOfBlocks = 1 hours / 15;
     //if uint winningPlayer in playBet() returns 1, winner is player1
     //if uint winningPlayer in playBet() returns 2, winner is player2
     
-    enum Bet {ROCK, PAPER, SCISSORS}
+    enum Bet {NULL, ROCK, PAPER, SCISSORS}
     
     struct BetBox {
        address player1;
@@ -36,85 +38,93 @@ contract RockPaperScissors {
         owner = msg.sender;
     }
     
-    function getGameID(bytes32 passPlayer1, bytes32 passPlayer2) internal pure returns(bytes32 gameID) {
+    function getGameID(bytes32 passPlayer1, bytes32 passPlayer2) public pure returns(bytes32 gameID) {
         return keccak256(passPlayer1, passPlayer2);
     }
     
-    function createBet(bytes32 passPlayer1, address player2, uint numberOfBlocks) public payable returns(bool success) {
-        require(betStructs[passPlayer1].amountPlayer1 == 0);
+    function createBet(bytes32 gameID, address player2, uint numberOfBlocks) public payable returns(bool success) {
+        require(betStructs[gameID].amountPlayer1 == 0);
         require(msg.sender != player2); 
-        betStructs[passPlayer1].player1 = msg.sender;
-        betStructs[passPlayer1].player2 = player2;
-        betStructs[passPlayer1].joinDeadline = block.number + numberOfBlocks;
-        betStructs[passPlayer1].amountPlayer1 = msg.value;
+        betStructs[gameID].player1 = msg.sender;
+        betStructs[gameID].player2 = player2;
+        betStructs[gameID].joinDeadline = block.number + numberOfBlocks;
+        betStructs[gameID].amountPlayer1 = msg.value;
         LogCreateBet(msg.sender, msg.value, numberOfBlocks);
         return true;
     }
     
-    function joinBet(bytes32 passPlayer1, uint nextNumberOfBlocks) public payable returns(bool success) {
-        require(betStructs[passPlayer1].amountPlayer2 == 0);
-        require(betStructs[passPlayer1].joinDeadline > block.number);
-        require(betStructs[passPlayer1].player2 == msg.sender); 
-        require(betStructs[passPlayer1].amountPlayer1 == msg.value);
-        betStructs[passPlayer1].playersNextMoveDeadline = block.number + nextNumberOfBlocks;
-        betStructs[passPlayer1].amountPlayer2 = msg.value;
+    function joinBet(bytes32 gameID, uint nextNumberOfBlocks) public payable returns(bool success) {
+        require(betStructs[gameID].amountPlayer2 == 0);
+        require(betStructs[gameID].joinDeadline > block.number);
+        require(betStructs[gameID].player2 == msg.sender); 
+        require(betStructs[gameID].amountPlayer1 == msg.value);
+        require(nextNumberOfBlocks < 1 days / 15);
+        require(nextNumberOfBlocks > 1 hours / 15);
+        betStructs[gameID].joinDeadline = 0;
+        betStructs[gameID].playersNextMoveDeadline = block.number + nextNumberOfBlocks;
+        betStructs[gameID].amountPlayer2 = msg.value;
         LogJoinBet(msg.sender, msg.value, nextNumberOfBlocks);
         return true;
     }
     
-    function player1Move(bytes32 passPlayer1, Bet betPlayer1) internal returns(bool success) {
-        require(betStructs[passPlayer1].player1 == msg.sender);
-        require(betStructs[passPlayer1].betPlayer1 == Bet.ROCK || betStructs[passPlayer1].betPlayer1 == Bet.PAPER || betStructs[passPlayer1].betPlayer1 == Bet.SCISSORS);
-        require(betStructs[passPlayer1].playersNextMoveDeadline > block.number);
+    function player1Move(bytes32 gameID, Bet betPlayer1) public returns(bool success) {
+        require(betStructs[gameID].player1 == msg.sender);
+        require(Bet(betPlayer1) == Bet.ROCK || Bet(betPlayer1) == Bet.PAPER || Bet(betPlayer1) == Bet.SCISSORS);
+        require(Bet(betPlayer1) != Bet.NULL); 
+        require(betStructs[gameID].playersNextMoveDeadline > block.number);
+        betStructs[gameID].betPlayer1 = Bet(betPlayer1);
         LogPlayer1Move(msg.sender, betPlayer1);
         return true;
     }
     
-    function player2Move(bytes32 passPlayer1, Bet betPlayer2) internal returns(bool success) {
-        require(betStructs[passPlayer1].player2 == msg.sender);
-        require(betStructs[passPlayer1].betPlayer2 == Bet.ROCK || betStructs[passPlayer1].betPlayer2 == Bet.PAPER || betStructs[passPlayer1].betPlayer2 == Bet.SCISSORS);
-        require(betStructs[passPlayer1].playersNextMoveDeadline > block.number);
+    function player2Move(bytes32 gameID, Bet betPlayer2) public returns(bool success) {
+        require(betStructs[gameID].player2 == msg.sender);
+        require(Bet(betPlayer2) == Bet.ROCK || Bet(betPlayer2) == Bet.PAPER || Bet(betPlayer2) == Bet.SCISSORS);
+        require(Bet(betPlayer2) != Bet.NULL); 
+        require(betStructs[gameID].playersNextMoveDeadline > block.number);
+        betStructs[gameID].betPlayer2 = Bet(betPlayer2);
         LogPlayer2Move(msg.sender, betPlayer2);
         return true;
     }
     
-    function playBet(bytes32 passPlayer1, Bet betPlayer1, Bet betPlayer2) public returns(uint winningPlayer) {
-        require(owner == msg.sender);
-        betStructs[passPlayer1].betPlayer1 = Bet(betPlayer1);
-        betStructs[passPlayer1].betPlayer2 = Bet(betPlayer2);
-        LogPlayBet(msg.sender, betPlayer1, betPlayer2);
-        if (betPlayer1 == betPlayer2) revert();
-        if ((betPlayer1 == Bet.PAPER && betPlayer2 == Bet.ROCK)||
-            (betPlayer1 == Bet.ROCK && betPlayer2 == Bet.SCISSORS)||
-            (betPlayer1 == Bet.SCISSORS && betPlayer2 == Bet.PAPER)||
-            (betPlayer1 == Bet.ROCK && betPlayer2 == Bet.SCISSORS)||
-            (betPlayer1 == Bet.PAPER && betPlayer2 == Bet.ROCK)||
-            (betPlayer1 == Bet.SCISSORS && betPlayer2 == Bet.PAPER)) return 1;
-        if ((betPlayer2 == Bet.PAPER && betPlayer1 == Bet.ROCK)||
-            (betPlayer2 == Bet.ROCK && betPlayer1 == Bet.SCISSORS)||
-            (betPlayer2 == Bet.SCISSORS && betPlayer1 == Bet.PAPER)||
-            (betPlayer2 == Bet.ROCK && betPlayer1 == Bet.SCISSORS)||
-            (betPlayer2 == Bet.PAPER && betPlayer1 == Bet.ROCK)||
-            (betPlayer2 == Bet.SCISSORS && betPlayer1 == Bet.PAPER)) return 2;  
+    function playBet(bytes32 gameID) public view returns(uint winningPlayer) {
+        if (betStructs[gameID].betPlayer1 == betStructs[gameID].betPlayer2) revert();
+        if ((betStructs[gameID].betPlayer1 == Bet.PAPER && betStructs[gameID].betPlayer2 == Bet.ROCK)||
+            (betStructs[gameID].betPlayer1 == Bet.ROCK && betStructs[gameID].betPlayer2 == Bet.SCISSORS)||
+            (betStructs[gameID].betPlayer1 == Bet.SCISSORS && betStructs[gameID].betPlayer2 == Bet.PAPER)||
+            (betStructs[gameID].betPlayer1 == Bet.ROCK && betStructs[gameID].betPlayer2 == Bet.SCISSORS)||
+            (betStructs[gameID].betPlayer1 == Bet.PAPER && betStructs[gameID].betPlayer2 == Bet.ROCK)||
+            (betStructs[gameID].betPlayer1 == Bet.SCISSORS && betStructs[gameID].betPlayer2 == Bet.PAPER)) return 1;
+        if ((betStructs[gameID].betPlayer2 == Bet.PAPER && betStructs[gameID].betPlayer1 == Bet.ROCK)||
+            (betStructs[gameID].betPlayer2 == Bet.ROCK && betStructs[gameID].betPlayer1 == Bet.SCISSORS)||
+            (betStructs[gameID].betPlayer2 == Bet.SCISSORS && betStructs[gameID].betPlayer1 == Bet.PAPER)||
+            (betStructs[gameID].betPlayer2 == Bet.ROCK && betStructs[gameID].betPlayer1 == Bet.SCISSORS)||
+            (betStructs[gameID].betPlayer2 == Bet.PAPER && betStructs[gameID].betPlayer1 == Bet.ROCK)||
+            (betStructs[gameID].betPlayer2 == Bet.SCISSORS && betStructs[gameID].betPlayer1 == Bet.PAPER)) return 2;  
         assert(false);
     }
     
-    function awardWinner(bytes32 passPlayer1, address winner) public returns(bool success) {
+    function awardWinner(bytes32 gameID, address winner) public returns(bool success) {
         require(owner == msg.sender);
-        betStructs[passPlayer1].winner = winner;
+        betStructs[gameID].winner = winner;
+        betStructs[gameID].playersNextMoveDeadline = 0;
         LogAwardWinner(msg.sender, winner);
         return true;
     }
 
-    function awardBetToWinner(bytes32 gameID, bytes32 passPlayer1, bytes32 passPlayer2) public returns(bool success) {
-        gameID = getGameID(passPlayer1, passPlayer2);
-        require(owner == msg.sender);
-        betStructs[passPlayer1].amountWinner = betStructs[passPlayer1].amountPlayer1 + betStructs[passPlayer1].amountPlayer2;
-        uint amount = betStructs[passPlayer1].amountWinner;
+    function awardBetToWinner(bytes32 passPlayer1, bytes32 passPlayer2) public returns(bool success) {
+        bytes32 gameID = getGameID(passPlayer1, passPlayer2);
+        betStructs[gameID].amountWinner = betStructs[passPlayer1].amountPlayer1 + betStructs[passPlayer1].amountPlayer2;
+        uint amount = betStructs[gameID].amountWinner;
         require(amount != 0);
-        betStructs[passPlayer1].amountWinner = 0;
+        betStructs[gameID].amountWinner = 0;
+        betStructs[gameID].amountPlayer1 = 0;
+        betStructs[gameID].amountPlayer2 = 0;
+        betStructs[gameID].player1 = 0x0;
+        betStructs[gameID].player2 = 0x0;
+        betStructs[gameID].winner = 0x0; 
         LogAwardBet(amount, msg.sender);
-        betStructs[passPlayer1].winner.transfer(amount);
+        betStructs[gameID].winner.transfer(amount);
         return true;    
     }
 }
